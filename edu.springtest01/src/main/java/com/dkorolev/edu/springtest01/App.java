63cb3778882f128bd0453cef72b2a35abf549f2c
Denis Korolev <denk666@gmail.com>
2019-04-11 17:07:39 +0300

    New Spring project

---
+++ edu.springtest01/src/main/java/com/dkorolev/edu/springtest01/App.java
@@ -0,0 +1,13 @@
+package com.dkorolev.edu.springtest01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
