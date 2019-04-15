c31aa88a0e83e3730de620af445ae0c0be9b65fc
Denis Korolev <denk666@gmail.com>
2019-04-15 18:28:40 +0300

    springtest02 init

---
+++ springtest02/src/main/java/com/dkorolev/edu/spring/springtest02/App.java
@@ -0,0 +1,13 @@
+package com.dkorolev.edu.spring.springtest02;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
